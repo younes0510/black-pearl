d2d7760f586fe08517b0040a0753e128b04cf852
testname <test@gmail.com>
2025-02-19 17:26:20 +0100

    black-pearl

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -10,7 +10,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-white">
+      <body >
 
          <Navbar />
         {children}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,23 @@
 export default function Home() {
-    return (
-        <>
-            <p className="text-red-400">dfsf</p>
-        </>
-    );
+  return (
+      <div className="bg-[#1d232a]">
+          <div className="relative">
+              <img src="black.jpg" className="w-full h-[550px] object-cover" />
+              <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
+                  <h1 className="text-7xl font-bold ">Black Pearl</h1>
+                  <button className="mt-4 px-4 py-2 bg-white text-black rounded cursor-pointer">Decouvrir</button>
+              </div>
+          </div>
+
+          <div className="text-center text-white text-3xl my-7 font-bold">
+    <h1>NOTRE MENU</h1>
+
+<div className="grid grid-cols-3">
+
+
+
+</div>
+          </div>
+      </div>
+  );
 }
