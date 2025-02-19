49f189842508e21a2cfb118e422396172fbb6d8d
aslxlce <aslxlce@outlook.com>
2025-02-19 17:56:44 +0100

    subheeero

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -2,21 +2,16 @@
 
 import { Navbar } from "@/components/Navbar";
 import { Footer } from "@/components/Footer";
+import SubHero from "@/components/SubHero";
 import "./globals.css";
 
 export default function RootLayout({ children }) {
-<<<<<<< HEAD
     return (
-        <html lang="en">
-            <body className="bg-white">
+        <html lang="en" data-theme="dark">
+            <body className="bg-white ">
                 <Navbar />
                 {children}
-=======
-  return (
-    <html lang="en">
-      <body >
->>>>>>> d2d7760f586fe08517b0040a0753e128b04cf852
-
+                <SubHero />
                 <Footer />
             </body>
         </html>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,28 +1,21 @@
 export default function Home() {
-<<<<<<< HEAD
-    return <></>;
+    return (
+        <div className="bg-[#1d232a]">
+            <div className="relative">
+                <img src="black.jpg" className="w-full h-[550px] object-cover" />
+                <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
+                    <h1 className="text-7xl font-bold ">Black Pearl</h1>
+                    <button className="mt-4 px-4 py-2 bg-white text-black rounded cursor-pointer">
+                        Decouvrir
+                    </button>
+                </div>
+            </div>
+
+            <div className="text-center text-white text-3xl my-7 font-bold">
+                <h1>NOTRE MENU</h1>
+
+                <div className="grid grid-cols-3"></div>
+            </div>
+        </div>
+    );
 }
-=======
-  return (
-      <div className="bg-[#1d232a]">
-          <div className="relative">
-              <img src="black.jpg" className="w-full h-[550px] object-cover" />
-              <div className="absolute inset-0 flex flex-col items-center justify-center text-white">
-                  <h1 className="text-7xl font-bold ">Black Pearl</h1>
-                  <button className="mt-4 px-4 py-2 bg-white text-black rounded cursor-pointer">Decouvrir</button>
-              </div>
-          </div>
-
-          <div className="text-center text-white text-3xl my-7 font-bold">
-    <h1>NOTRE MENU</h1>
-
-<div className="grid grid-cols-3">
-
-
-
-</div>
-          </div>
-      </div>
-  );
-}
->>>>>>> d2d7760f586fe08517b0040a0753e128b04cf852
